391767c4703103e8a76b80b98173ce63422441f2
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-07-26 22:33:05 +0530

    Move business controller to business controllers directory

--- app/Http/Controllers/Business/BusinessController.php
+++ app/Http/Controllers/Business/BusinessController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Business;
 
 use App\Models\Business;
 use Illuminate\Http\Request;
@@ -14,7 +14,6 @@ class BusinessController extends Controller
      */
     public function index()
     {
-        //
     }
 
     /**
@@ -24,62 +23,61 @@ class BusinessController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Business  $business
+     * @param \App\Models\Business $business
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(Business $business)
     {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Business  $business
+     * @param \App\Models\Business $business
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit(Business $business)
     {
-        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Business  $business
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Business     $business
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Business $business)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Business  $business
+     * @param \App\Models\Business $business
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy(Business $business)
     {
-        //
     }
 }
