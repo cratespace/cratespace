da04bd3415d0fbcfc959fe7d6e432e55761345f3
thavarshan <tjthavarshan@gmail.com>
2020-08-14 00:23:41 +0530

    Add email template UI design

--- resources/views/emails/orders/placed.blade.php
+++ resources/views/emails/orders/placed.blade.php
@@ -1,12 +1,18 @@
 @component('mail::message')
-# Introduction
+# Order Confirmation
 
-The body of your message.
+## Hi {{ $order->name }}, Thank you for your order!
+
+We've received your order and will contact you as soon as your order is approved by the respective logistics service. In the case your order is rejected, you will be provided with a **full** refund.
+
+You can track your order status by clicking the button below.
 
 @component('mail::button', ['url' => route('orders.confirmation', ['confirmationNumber' => $order->confirmation_number])])
 View order status
 @endcomponent
 
+@component('mail::footer')
 Thanks,<br>
-{{ config('app.name') }}
+{{ config('app.name') }} Team
+@endcomponent
 @endcomponent
