a89b5cf5417ccc5fc522f79331aab286e4cdf5bf
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-08-29 00:40:51 +0530

    Update _email.blade.php

--- resources/views/components/forms/fields/_email.blade.php
+++ resources/views/components/forms/fields/_email.blade.php
@@ -1,7 +1,7 @@
 <label class="block">
     <span class="text-gray-700 text-sm font-semibold">{{ $label ?? 'Email' }}</span>
 
-    <input type="email" name="email" id="email" class="form-input mt-1 block w-full @error('email') placeholder-red-500 border-red-300 bg-red-100 @enderror" placeholder="john.doe@example.com" required value="{{ old('email') ?? ($email ?? null) }}">
+    <input type="email" name="email" id="email" pattern=".+@.+\..+" class="form-input mt-1 block w-full @error('email') placeholder-red-500 border-red-300 bg-red-100 @enderror" placeholder="john.doe@example.com" required value="{{ old('email') ?? ($email ?? null) }}">
 </label>
 
 @error('email')
