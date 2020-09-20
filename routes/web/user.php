1a4ac2fb150aff0a1e01892c1e55097409126dbf
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-09-20 11:14:39 +0530

    Update user.php

--- routes/web/user.php
+++ routes/web/user.php
@@ -2,16 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\OtherBrowserSessionsController;
 
-/*
- * User Account/Profile Resource Route...
- */
-Route::resource('/users', 'Auth\ProfileController');
+Route::group(['middleware' => ['auth', 'verified']], function (): void {
+    /*
+     * User Account/Profile Resource Route...
+     */
+    Route::resource('/users', 'Auth\ProfileController');
 
-/*
- * User Password Reset Route.
- */
-Route::put(
-    '/users/{user}/update/password',
-    [ResetPasswordController::class, '@update']
-)->name('users.password');
+    /*
+     * User Password Reset Route.
+     */
+    Route::put(
+        '/users/{user}/update/password',
+        [ResetPasswordController::class, '@update']
+    )->name('users.password');
+
+    /*
+     * Other Browser Sessions Invalidation Route.
+     */
+    Route::delete(
+        '/user/other-browser-sessions',
+        [OtherBrowserSessionsController::class, 'destroy']
+    )->name('other-browser-sessions.destroy');
+
+    /*
+     * User Profile Photo Controller.
+     */
+    Route::delete(
+        '/user/profile-photo',
+        [ProfilePhotoController::class, 'destroy']
+    )->name('current-user-photo.destroy');
+});
