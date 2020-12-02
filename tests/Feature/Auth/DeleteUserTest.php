291de2b866c75ed1ce39b4a32ee31aa365519160
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2020-12-02 08:52:51 +0530

    Add delete user tests

--- tests/Feature/Auth/DeleteUserTest.php
+++ tests/Feature/Auth/DeleteUserTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Auth;
 
 use Tests\TestCase;
 use App\Models\User;
+use App\Jobs\DeleteUserJob;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -31,6 +32,22 @@ class DeleteUserTest extends TestCase
         $user = create(User::class);
 
         $response = $this->actingAs($user)->deleteJson('/user');
+
+        $response->assertStatus(204);
+    }
+
+    /** @test */
+    public function user_account_delete_request_is_pushed_to_queue()
+    {
+        Queue::fake();
+
+        $user = create(User::class);
+
+        Queue::assertNothingPushed();
+
+        $response = $this->actingAs($user)->deleteJson('/user');
+
+        Queue::assertPushed(DeleteUserJob::class);
 
         $response->assertStatus(204);
     }
