1246ef47f9cae5815df3e32d92941a828bb6570c
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-16 23:23:12 +0530

    Add update business info route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\BusinessController;
 
 Route::get('/', fn () => Inertia::render('Marketing/Welcome'))->name('welcome');
 
-Route::middleware(['auth:sentinel', 'verified'])->get(
-    '/home',
-    fn () => Inertia::render('Business/Home')
-)->name('home');
+Route::group([
+    'middleware' => ['auth:sentinel', 'verified'],
+], function (): void {
+    Route::get('/home', fn () => Inertia::render('Business/Home'))->name('home');
+
+    Route::put('/user/business', [BusinessController::class, '__invoke'])->name('user.business');
+});
