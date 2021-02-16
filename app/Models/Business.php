3e4df04513b4243cf7d19780bb9c447dd739cbbc
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-16 22:57:37 +0530

    Add business model, factory and migration

---
+++ app/Models/Business.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Cratespace\Sentinel\Models\Traits\HasProfilePhoto;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Business extends Model
+{
+    use HasFactory;
+    use HasProfilePhoto;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+        'about',
+        'street',
+        'city',
+        'state',
+        'country',
+        'postcode',
+        'user_id',
+        'profile_photo_path',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    /**
+     * Get the user the business belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ database/migrations/2021_02_16_172134_create_businesses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusinessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('businesses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->mediumText('about')->nullable();
+            $table->string('street')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('country')->nullable();
+            $table->string('zipcode')->nullable();
+            $table->text('profile_photo_path')->nullable();
+            $table->foreignId('user_id')->constrained('users', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('businesses');
+    }
+}
