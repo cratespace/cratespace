6e2771d742a66fdb5ae5e08de7fc0c8fe6bfd899
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-17 12:54:21 +0530

    Add presenter unit tests

--- tests/Unit/Presenters/PresenterTest.php
+++ tests/Unit/Presenters/PresenterTest.php
@@ -2,17 +2,36 @@
 
 namespace Tests\Unit\Presenters;
 
+use InvalidArgumentException;
+use Tests\Fixtures\MockModel;
 use PHPUnit\Framework\TestCase;
+use Tests\Fixtures\MockPresenter;
 
 class PresenterTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    protected $model;
+
+    protected $presenter;
+
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $this->model = new MockModel();
+        $this->model->foo = 'bar';
+        $this->presenter = new MockPresenter($this->model);
+    }
+
+    public function testCanPresentMutatedModelAttribute()
+    {
+        $this->assertEquals('Bar', $this->presenter->foobar);
+    }
+
+    public function testThrowsExceptionIfPresenterMethodUndefined()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Tests\Fixtures\MockPresenter does not respond to the property or method "barbaz"');
+
+        $this->presenter->barbaz;
     }
 }
