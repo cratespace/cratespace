fa852511f9bde02670709ccfd68b6567dee07a96
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-22 03:33:18 +0530

    Add tests for hascode

---
+++ tests/Unit/Codes/HasCodeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit\Codes;
+
+use Tests\TestCase;
+use App\Codes\HashCode;
+
+class HasCodeTest extends TestCase
+{
+    public function testCodesGeneratedAreAtLeast6CharactersLong()
+    {
+        $this->assertTrue(strlen(HashCode::generate(1)) >= 6);
+    }
+
+    public function testCodesGeneratedCanOnlyContainUppercaseLetters()
+    {
+        $this->assertMatchesRegularExpression('/^[A-Z]+$/', HashCode::generate(1));
+    }
+
+    public function testCodesGeneratedForTheSameResourceIdAreTheSame()
+    {
+        $code1 = HashCode::generate(1);
+
+        $code2 = HashCode::generate(1);
+
+        $this->assertEquals($code1, $code2);
+    }
+
+    public function testCodesGeneratedForDifferentResourceIdsAreDifferent()
+    {
+        $code1 = HashCode::generate(1);
+
+        $code2 = HashCode::generate(2);
+
+        $this->assertNotEquals($code1, $code2);
+    }
+}
