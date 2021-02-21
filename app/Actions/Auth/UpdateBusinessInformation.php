c46fbeb6a9d49b73b4d9f6ade41349bd7db9e6a2
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-21 16:25:11 +0530

    Add complete tests for business profile

--- app/Actions/Auth/UpdateBusinessInformation.php
+++ app/Actions/Auth/UpdateBusinessInformation.php
@@ -2,7 +2,7 @@
 
 namespace App\Actions\Auth;
 
-use Illuminate\Contracts\Auth\Authenticatable;
+use App\Models\User;
 use Cratespace\Sentinel\Contracts\Actions\UpdatesUserProfiles;
 
 class UpdateBusinessInformation implements UpdatesUserProfiles
@@ -10,12 +10,12 @@ class UpdateBusinessInformation implements UpdatesUserProfiles
     /**
      * Validate and update the given user's profile information.
      *
-     * @param \Illuminate\Contracts\Auth\Authenticatable $user
-     * @param array                                      $data
+     * @param \App\Models\User $user
+     * @param array            $data
      *
      * @return void
      */
-    public function update(Authenticatable $user, array $data): void
+    public function update(User $user, array $data): void
     {
         if (isset($data['photo'])) {
             $user->business->updateProfilePhoto($data['photo']);
