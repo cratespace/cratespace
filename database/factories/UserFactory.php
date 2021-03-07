08808bc5875ec3ede68d57cc74ae5f7c9da27000
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-03-07 22:37:15 +0530

    Updgrade to preflight 3.3

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
 {
@@ -22,26 +22,32 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        if (! is_null($details = $this->defaultUserDetails())) {
+            return $details;
+        }
+
         return [
             'name' => $this->faker->name,
+            'username' => $this->faker->unique()->userName,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'settings' => [],
+            'locked' => false,
+            'profile_photo_path' => null,
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Get preflight set default user details.
      *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     * @return array|null
      */
-    public function unverified()
+    protected function defaultUserDetails(): ?array
     {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
+        return config('defaults.users.credentials', null);
     }
 }
