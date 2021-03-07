d21f8eb2544fa45b3f8cb0e1814b35a5adf17124
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-03-08 01:07:46 +0530

    WIP

--- .github/ISSUE_TEMPLATE/BUG_REPORT.md
+++ .github/ISSUE_TEMPLATE/BUG_REPORT.md
@@ -6,7 +6,7 @@ about: "Report something that's broken."
 <!-- DO NOT THROW THIS AWAY -->
 <!-- Fill out the FULL versions with patch versions -->
 
-- App Version: 1.0.0
+- App Version: 2.0.0
 - PHP Version: 7.4.13
 - Database Driver & Version: MySQL 5.7
 
--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -13,7 +12,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Cratespace'),
 
     /*
     |--------------------------------------------------------------------------
@@ -135,9 +134,8 @@ return [
     */
 
     'providers' => [
-
         /*
-         * Laravel Framework Service Providers...
+         * Cratespace Framework Service Providers...
          */
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
@@ -176,7 +174,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\SentinelServiceProvider::class,
         App\Providers\PreflightServiceProvider::class,
-
     ],
 
     /*
@@ -191,7 +188,6 @@ return [
     */
 
     'aliases' => [
-
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
@@ -228,7 +224,5 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
     ],
-
 ];
