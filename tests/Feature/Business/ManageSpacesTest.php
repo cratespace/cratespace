53e40d59fe9c4a5732a666c140ae3271cd4e8ecc
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-03-09 20:07:22 +0530

    Add complete update space tests

--- tests/Feature/Business/ManageSpacesTest.php
+++ tests/Feature/Business/ManageSpacesTest.php
@@ -2,21 +2,223 @@
 
 namespace Tests\Feature\Business;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Space;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Cratespace\Preflight\Testing\Contracts\Postable;
 
-class ManageSpacesTest extends TestCase
+class ManageSpacesTest extends TestCase implements Postable
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function testUpdateSpaceDetails()
     {
-        $response = $this->get('/');
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters());
+
+        $response->assertStatus(303);
+        $response->assertSessionHasNoErrors();
+    }
+
+    public function testUpdateSpaceDetailsThroughJson()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->putJson("/spaces/{$space->code}", $this->validParameters());
 
         $response->assertStatus(200);
+        $response->assertSessionHasNoErrors();
+    }
+
+    public function testOnlyAuthenticatedUsersCanUpdateSpaceDetails()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+        auth()->logout();
+
+        $this->assertGuest();
+
+        $response = $this->putJson("/spaces/{$space->code}", $this->validParameters());
+
+        $response->assertStatus(401);
+    }
+
+    public function testGuestUsersAreRedirectedToLoginPage()
+    {
+        $response = $this->put('/spaces/DJFLAUSDLASJDLHJSA', $this->validParameters());
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function testRequiresValidDepartsAt()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'departs_at' => '',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('departs_at');
+    }
+
+    public function testRequiresValidArrivesAt()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'arrives_at' => '',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('arrives_at');
+    }
+
+    public function testRequiresValidOrigin()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'origin' => '',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('origin');
+    }
+
+    public function testRequiresValidDestination()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'destination' => '',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('destination');
+    }
+
+    public function testRequiresValidHeight()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'height' => '',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('height');
+    }
+
+    public function testRequiresValidLength()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'length' => '',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('length');
+    }
+
+    public function testRequiresValidWidth()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'width' => '',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('width');
+    }
+
+    public function testRequiresValidWeight()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'weight' => '',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('weight');
+    }
+
+    public function testRequiresValidPrice()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'price' => 'sadasjndajs',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('price');
+    }
+
+    public function testRequiresValidTax()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'tax' => 'sadasjndajs',
+        ]));
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('tax');
+    }
+
+    public function testTaxIsOptional()
+    {
+        $this->signIn($user = User::factory()->withBusiness()->create());
+        $space = create(Space::class, ['user_id' => $user->id]);
+
+        $response = $this->put("/spaces/{$space->code}", $this->validParameters([
+            'tax' => '',
+        ]));
+
+        $response->assertStatus(303);
+    }
+
+    /**
+     * Provide only the necessary paramertes for a POST-able type request.
+     *
+     * @param array $overrides
+     *
+     * @return array
+     */
+    public function validParameters(array $overrides = []): array
+    {
+        return array_merge([
+            'departs_at' => now()->addMonths(rand(1, 2)),
+            'arrives_at' => now()->addMonths(rand(3, 4)),
+            'origin' => $this->faker->city,
+            'destination' => $this->faker->city,
+            'height' => rand(1, 9),
+            'width' => rand(1, 9),
+            'length' => rand(1, 9),
+            'weight' => rand(1, 9),
+            'note' => $this->faker->sentence(7),
+            'price' => 10.00,
+            'tax' => 0.50,
+            'type' => $this->faker->randomElement(['Local', 'International']),
+        ], $overrides);
     }
 }
