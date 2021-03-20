7e8fd2f5cecf8a478daf2b0c79a9654404083f63
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-03-20 11:00:49 +0530

    Add complete tests for value objects

--- app/Models/Values/Value.php
+++ app/Models/Values/Value.php
@@ -23,9 +23,9 @@ abstract class Value
     /**
      * Create new instance of value object.
      *
-     * @param array $details
+     * @param array|null $details
      */
-    public function __construct(array $details)
+    public function __construct(?array $details = null)
     {
         $this->details = $details;
     }
@@ -36,17 +36,51 @@ abstract class Value
      * @param string $value
      *
      * @return mixed
+     *
+     * @throws \InvalidArgumentException
      */
     public function __get(string $value)
     {
-        if (property_exists($this, 'values') && in_array($value, $this->values)) {
-            return $this->details[$value];
+        if (! empty($this->values)) {
+            return $this->getWithConstraints($value);
         }
 
         if (isset($this->details[$value])) {
             return $this->details[$value];
         }
 
-        throw new InvalidArgumentException("Property [{$value}] does not exist");
+        $this->throwPropertyDoesNotExist($value);
+    }
+
+    /**
+     * Get value of key included within $values property.
+     *
+     * @param string $value
+     *
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function getWithConstraints(string $value)
+    {
+        if (in_array($value, $this->values)) {
+            return $this->details[$value];
+        }
+
+        $this->throwPropertyDoesNotExist($value);
+    }
+
+    /**
+     * Throw property does not exist exception.
+     *
+     * @param string $property
+     *
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function throwPropertyDoesNotExist(string $property): void
+    {
+        throw new InvalidArgumentException("Property [{$property}] does not exist");
     }
 }
---
+++ tests/Fixtures/ValueConstraintsStub.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\Fixtures;
+
+use App\Models\Values\Value;
+
+class ValueConstraintsStub extends Value
+{
+    protected $values = ['name'];
+}
