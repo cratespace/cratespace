b151f3cd54104ff3bc143c6f7576eeba196eeb25
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-03-20 13:20:47 +0530

    Update default user seeder class

--- database/seeders/DefaultUserSeeder.php
+++ database/seeders/DefaultUserSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
+use Cratespace\Preflight\Models\Role;
+use Cratespace\Preflight\Models\Permission;
 
 class DefaultUserSeeder extends Seeder
 {
@@ -14,6 +17,58 @@ class DefaultUserSeeder extends Seeder
      */
     public function run()
     {
-        User::create(config('defaults.users.credentials'));
+        $user = $this->createUser();
+
+        $this->assignDefualtRoles($user);
+    }
+
+    /**
+     * Create a user with the given default credentials.
+     *
+     * @return \App\Models\User
+     */
+    protected function createUser(): User
+    {
+        return User::create(config('defaults.users.credentials'));
+    }
+
+    /**
+     * Assign all default roles to the user.
+     *
+     * @param \App\Models\User $user
+     *
+     * @return void
+     */
+    protected function assignDefualtRoles(User $user): void
+    {
+        $roles = $this->createRolesWithPermissions();
+
+        $roles->each(function (Role $role) use ($user): void {
+            $user->assignRole($role);
+        });
+    }
+
+    /**
+     * Create and allow roles with relative permissions.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    protected function createRolesWithPermissions(): Collection
+    {
+        return collect(config('defaults.users.roles'))->map(function (array $role): Role {
+            $role = Role::create($role);
+
+            $permissions = config("defaults.users.permissions.{$role->name}");
+
+            if (! is_null($permissions)) {
+                collect($permissions)->each(function (string $permission) use ($role): void {
+                    $permission = Permission::create(['label' => $permission]);
+
+                    $role->allowTo($permission);
+                });
+            }
+
+            return $role;
+        })->values();
     }
 }
