467000112d10fbd3387bf8889c55b5dd09a3ee42
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-04-03 20:31:42 +0530

    Update dependencies

--- app/Observers/SpaceObserver.php
+++ app/Observers/SpaceObserver.php
@@ -3,22 +3,9 @@
 namespace App\Observers;
 
 use App\Models\Space;
-use App\Products\Manifest;
 
 class SpaceObserver
 {
-    /**
-     * Create new instance of SpaceObserver.
-     *
-     * @param \App\Products\Manifest $manifest
-     *
-     * @return void
-     */
-    public function __construct(Manifest $manifest)
-    {
-        $this->manifest = $manifest;
-    }
-
     /**
      * Handle the Space "created" event.
      *
@@ -28,6 +15,6 @@ class SpaceObserver
      */
     public function created(Space $space): void
     {
-        $this->manifest->store($space);
+        $space->product()->create(['code' => $space->code]);
     }
 }
